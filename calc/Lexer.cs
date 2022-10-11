4e1a88449496556ab756456070fc6da63dbbf7e4
Jakub Antonín Štigler <jakubstig@outlook.com>
2022-10-11 20:01:27 +0200

    Add project files.

---
+++ calc/Lexer.cs
@@ -0,0 +1,20 @@
+﻿namespace calc;
+
+internal class Lexer
+{
+    TextReader _input;
+    int _cur;
+    int Cur => _cur;
+    public double NuberToken { get; private set; }
+    public string StringToken { get; private set; }
+
+
+    public Lexer(TextReader input)
+    {
+        _input = input;
+    }
+
+
+
+    private int NextChar() => _cur = _input.Read();
+}
